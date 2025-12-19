d46645eec8ea1e5110f0b428c301ee9406ba4fd7
Nikita Markin <mr.nik2014@gmail.com>
2025-12-19 13:55:14 +0300

    FEAT: base classes for external service integration - ExternalApiServiceBase - ExternalApiClientBase

---
+++ app/external_api/api_services/base/__init__.py
@@ -0,0 +1,26 @@
+from abc import ABC
+
+
+class ExternalApiServiceBase(ABC):
+    """Базовый класс для внешних API сервисов"""
+    NAME = ''
+
+    def __init__(self):
+        self.methods = None
+
+    @property
+    def name(self) -> str:
+        if not self.NAME:
+            raise ValueError(f'Не задано "NAME" для {self.__class__.__name__}')
+        return self.NAME
+
+    def has_method(self, method_name):
+        return hasattr(self.methods, method_name)
+
+    def execute(self, method_name, *args, **kwargs):
+        if not self.has_method(method_name):
+            raise ValueError(f'API сервис "{self.name}" не имеет метода {method_name}')
+
+        method = getattr(self.methods, method_name)
+        result = method(*args, **kwargs)
+        return result
